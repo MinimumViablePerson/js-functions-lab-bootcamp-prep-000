932d4fe9a0560c89aae86b3354f308a3b00fcad1
Nicolas Marcora <nicolas.marcora@yahoo.co.uk>
2018-07-28 19:15:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,4 @@
 const happyHolidays = () => 'Happy holidays!'
 const happyHolidaysTo = name => `Happy holidays, ${name}!`
 const happyHolidayTo = (holiday, name) => `Happy ${holiday}, ${name}!`
+const holidayCountdown = (holiday, days) => ``
