88c4f62df388c36f69532dd1f1c7be40e378a24d
Nicolas Marcora <nicolas.marcora@yahoo.co.uk>
2018-07-28 19:16:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 // write your code below!
-const happyHolidays = () => 'Happy holidays!'
-const happyHolidaysTo = name => `Happy holidays, ${name}!`
-const happyHolidayTo = (holiday, name) => `Happy ${holiday}, ${name}!`
-const holidayCountdown = (holiday, days) => `It's ${days} days until ${holiday}!`
+const happyHolidays = () =>
+  'Happy holidays!'
+  
+const happyHolidaysTo = name =>
+  `Happy holidays, ${name}!`
+
+const happyHolidayTo = (holiday, name) =>
+  `Happy ${holiday}, ${name}!`
+  
+const holidayCountdown = (holiday, days) =>
+  `It's ${days} days until ${holiday}!`
+  
